bce80e5d96a8bb84d279dbeba7958e38cb9572f3
Krishna Manaswi Digumarti <dkrishnamanaswi@gmail.com>
2016-04-29 05:19:12 +0100

    cleaned up code into one file

--- sim_single_uav.m
+++ sim_single_uav.m
@@ -3,7 +3,7 @@ function sim_single_uav
 % description: simulation of single uav moving to a specified target
 
 %% tabula rasa
-clear all
+% clear all
 close all
 clc
 
@@ -18,7 +18,7 @@ hold on;
 %% define time and time step
 t = 0; % [s]
 tMax = 1800; % [s] 30 minutes
-dt = 3.6; % [s]
+dt = 3; % [s]
 nSteps = tMax / dt;
 
 %% initialize state and control input
@@ -35,6 +35,8 @@ navMemory.state = 1;
 
 %% target
 target = [500;0];
+
+% store = zeros(2,nSteps);
 
 %% main simulaiton loop
 for k = 1:nSteps
@@ -53,19 +55,103 @@ for k = 1:nSteps
     % take measurement
     p = cloudsamp(cloud,X(1,1),X(2,1),t);
     
-    % clear the axes for fresh plotting
-    cla
+    % adjust target based on measurement
+    if p > 0.85 && p < 1.15
+        target = X(1:2,1);
+    end
     
-    % put information in the title
-    title(sprintf('t=%.1f secs pos=(%.1f, %.1f)  Concentration=%.2f',t, X(1,1),X(2,1),p))
-        
-    % plot 
+    % drawing
+    cla
+    title(sprintf('t=%.1f secs pos=(%.1f, %.1f)  Concentration=%.2f',t, X(1,1),X(2,1),p)) 
     plot(X(1,1),X(2,1),'o') % robot location
     plot(target(1,1), target(2,1), 'sg') % target
     
-    % plot the cloud contours
     cloudplot(cloud,t)
     
-    % pause ensures that the plots update
     pause(0.1)
+end
+
+end % end of main
+
+%% Helper Functions -------------------------------------------------------
+% -------------------------------------------------------------------------
+function [ X_next ] = simMove( X,U,dt )
+%simMove given current state, control input and time step, this function
+%returns the state at thenext time step
+%   Implements a simple 4th order Runge Kutta prediction
+
+k1 = continuousDynamics(X,U);
+k2 = continuousDynamics(X+k1*dt/2,U);
+k3 = continuousDynamics(X+k2*dt/2,U);
+k4 = continuousDynamics(X+k3*dt,U);
+
+X_next = X + (k1 + 2*k2 + 2*k3 + k4)*dt/6;
+
+end
+
+% -------------------------------------------------------------------------
+function [ X_dot ] = continuousDynamics( X,U )
+%CONTINUOUSDYNAMICS simulates continuous dynamics of the system
+%   Taken from the model of the UAV
+%   X = [x;y;theta], U = [v;mu]
+%   x' = v sin(theta)
+%   y' = v cos(theta)
+%   theta' = v mu
+
+X_dot = zeros(3,1);
+X_dot(1,1) = U(1,1) * sin( X(3,1) );
+X_dot(2,1) = U(1,1) * cos( X(3,1) );
+X_dot(3,1) = U(1,1) * U(2,1);
+
+end
+
+% -------------------------------------------------------------------------
+function [ Y ] = simGPS( X, navMemory, target )
+%SIMGPS simulates GPS measurements
+%   For the position, adds a gaussian noise of 3m
+%   for the heading, uses previous known position and computes arc tangent
+%   for heading to target, first find target orientation and then subtract
+%   agent's heading
+
+Y.position = X(1:2,1) + 3*randn(2,1);
+
+Y.heading = atan2(Y.position(1,1) - navMemory.lastPosition(1,1),...
+                  Y.position(2,1) - navMemory.lastPosition(2,1));
+              
+Y.headingToTarget = atan2(target(1,1) - navMemory.lastPosition(1,1),...
+                          target(2,1) - navMemory.lastPosition(2,1))...
+                    - Y.heading;
+
+% [Note: heading is measued from North in clockwise direction]
+end
+
+% -------------------------------------------------------------------------
+function [ U_new, navMemory ] = simNavDecision( Y, U, navMemory )
+%SIMNAVDECISION returns new velocity commands based on current estimated
+%state and internal memory
+
+% updae agent's memory
+navMemory.lastPosition = Y.position;
+navMemory.velocityCommands = U;
+
+% update velocity command based on current heading to target
+v_new = 10 * ((pi/2 - abs(Y.headingToTarget))/(pi/2));
+mu_new =  (3*pi/180) * (Y.headingToTarget/(pi/2));
+
+% apply limits on v
+if v_new > 20
+    v_new = 20;
+end
+
+if v_new < 10
+    v_new = 10;
+end
+
+% apply limits on mu
+if mu_new > 6*pi/180
+    mu_new = 6*pi/180;
+end
+
+U_new = [v_new; mu_new];
+
 end
